d6df9bfe939c953b684d92b8021e71aff0a93856
rohitameta706-max <rohitameta666@gmail.com>
2026-04-02 20:00:43 +0530

    Add user account management endpoints

--- src/contollers/user.controller.js
+++ src/contollers/user.controller.js
@@ -215,6 +215,23 @@ const refershAccessToken = asyncHandler(async(req, res) => {
    }
 })
 
+const changeCurrentPassword = asyncHandler(async(req, res) => {
+    const {oldPassword, newPassword} = req.body
+
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect =  await user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect){
+        throw new ApiError(400, "Invalid Old Password")
+    }
+
+    user.password = newPassword
+    user.save({validateBeforeSave: false})
+
+    return res.staus(200)
+    .json(new ApiResponse(200, {}, "Password Changed Successfully"))
+})
+
 export {
     registerUser,
     loginUser,
